65bead6e4b7ca6062114b8e699f50093afb1fd51
JWalters <joshwalters34@gmail.com>
2021-03-05 23:11:09 -0500

    added some html fields for weather values, added styling with CSS, added functionality to display weather data on my page.

--- Develop/script.js
+++ Develop/script.js
@@ -4,7 +4,7 @@ var cityHistory = document.querySelector("#cityHistory"); //location for history
 var weatherDisplay = document.querySelector("#weatherDisplay");
 var recentCities = document.querySelector("#recentCities"); //ul for city history to add onto
 var cities = [];
-
+var cityName = document.querySelector("#cityName");
 
 
 $("#searchBtn").on("click", function (event) {
@@ -24,81 +24,97 @@ $("#searchBtn").on("click", function (event) {
     searchText.value = "";
  
     storeCities();
-    displayCities();
+    // displayCities();
     // fetchWeatherData();
+    fetchLatLon(cityEntry);
+    cityName.textContent = cityEntry.toUpperCase();
+
+    var todayDate = moment().format('M/D/YYYY')
+    var date = document.querySelector("#date");
+    date.textContent = todayDate;
 
 })
 
-// fetchLatLon();
 
 
-
-
-// function fetch1(cityEntry) {fetch stuff
-//fetch2(data.coord.lat, data.coord.lon)}
 function fetchLatLon(cityEntry) {
     console.log(cityEntry);
-//    fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cityEntry}&appid=8145ecb6e9f22712784b4a7964038388`)
-   fetch ('https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=8145ecb6e9f22712784b4a7964038388')
-    // fetch ("https://api.openweathermap.org/data/2.5/weather?q=" + cityEntry + "&appid=8145ecb6e9f22712784b4a7964038388")
+
+    fetch ("https://api.openweathermap.org/data/2.5/weather?q=" + cityEntry + "&units=imperial&appid=8145ecb6e9f22712784b4a7964038388")
     .then(function(response) {
-        console.log(response);
+        // console.log(response);
+ //console.log(response.coord.lon);
+ //console.log(response.coord.lat);
         return response.json();
     }) .then(function(results) {
-        console.log(results)
-    })
-        .catch(function(error) {
-            console.error(error);
-        }) 
-        
-    //    fetchWeatherData(data.coord.lat, data.coord.lon);
-    //    fetchWeatherData(resuts);
+        console.log(results);
+        console.log(results.coord.lon);
+        console.log(results.coord.lat);
+        console.log(results.main.temp);
+        var lat = results.coord.lon;
+        var lon = results.coord.lat;
+        var tempEl = Math.round(results.main.temp);
+        var humidityEl = results.main.humidity;
+        var windEl = results.wind.speed;
 
+
+        var tempDisplay = document.querySelector("#tempDisplay");
+        tempDisplay.textContent = 'Temperature: ' + tempEl + 'F';
+
+        var humidityDisplay = document.querySelector("#humidityDisplay");
+        humidityDisplay.textContent = 'Humidity: ' + humidityEl + '%';
+
+        var windDisplay = document.querySelector('#windDisplay');
+        windDisplay.textContent = 'Wind Speed: ' + windEl + 'mph';
+        return fetch ("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=metric&exclude=hourly&appid=8145ecb6e9f22712784b4a7964038388");
+
+        
+        
+ }) .then(function(response){
+     return response.json();
+ }) .then(function(results) {
+     console.log(results)
+
+     var uvEl = results.current.uvi
+
+     var uvDisplay = document.querySelector("#uvResult");
+     uvDisplay.textContent = uvEl;
+
+     if(uvEl <= 3) {
+         document.getElementById("uvResult").style.backgroundColor = "green";
+     } else if(uvEl >= 4 && uvEl <= 7) {
+        document.getElementById("uvResult").style.backgroundColor = "yellow";
+     } else if(uvEl >= 8) {
+        document.getElementById("uvResult").style.backgroundColor = "red";
+     }
+
+
+ });    
 }
 
-//function fetch2(lat,lon) {fetch stuff}
-// function fetchWeatherData (results) {
-//     // var data = document.location.search;
-//     // fetch(`https://api.openweathermap.org/data/2.5/onecall?lat${lat}=&lon=${lon}&exclude=hourly,daily&appid=8145ecb6e9f22712784b4a7964038388`)
-//    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=41.85&lon=-87.65&exclude=hourly,daily&appid=8145ecb6e9f22712784b4a7964038388`)
-//     .then(function(response) {
-//         console.log(response);
-//         return response.json();
-//     }) .then(function(results) {
-//         console.log(results);
-//     })
-//         .catch(function(error) {
-//             console.error(error);
-//         })
-    
-//        var tempData = document.createElement('p');
+
+//   function displayWeather(results){
+//     var weatherData = document.createElement('p');
+//         weatherData.classList.add('bg-light', 'text-dark', 'my-2')
+
+//         var tempData = document.createElement('p');
 //        tempData.classList.add('bg-ligh', 'text-dark', 'my-2');
 //        tempData.innerHTML = "Temperature: " + results.current.temp;
 //        weatherDisplay.append(tempdata)
-// }
-
-// data.current.temp
-// fetch by city name to get lat and lon values
-
-    //then run the api call to get all of the weather data neededd 
-//   .then(fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid=8145ecb6e9f22712784b4a7964038388",))
-
-  function displayWeather(){
-    var weatherData = document.createElement('p');
-        weatherData.classList.add('bg-light', 'text-dark', 'my-2')
-  }
+//        console.log(results.current.temp)
+//   }
 
 //   function to display history of cities on left side
 function displayCities() {
     cityHistory.innerHTML = "";
 
-    for (var i = 0; i < cities.length; i++) {
+    for (var i = 0; i < 5; i++) {
         var city = cities[i];
-        var li = document.createElement("li");
-        li.textContent = city;
-        li.setAttribute("data-index", i);
+        var list = document.createElement("p");
+        list.textContent = city;
+        list.setAttribute("data-index", i);
 
-        recentCities.appendChild(li);
+        recentCities.append(list);
     }
 }
 // local storage get function to pull back recent cities
