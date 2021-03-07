a64cc1cc85856788d3afb2fdb13fe96113b82c75
JWalters <joshwalters34@gmail.com>
2021-03-06 22:14:48 -0500

    added code for 5-day weather

--- Develop/script.js
+++ Develop/script.js
@@ -5,6 +5,7 @@ var weatherDisplay = document.querySelector("#weatherDisplay");
 var recentCities = document.querySelector("#recentCities"); //ul for city history to add onto
 var cities = [];
 var cityName = document.querySelector("#cityName");
+var todayDate = moment().format('M/D/YYYY')
 
 
 $("#searchBtn").on("click", function (event) {
@@ -27,9 +28,10 @@ $("#searchBtn").on("click", function (event) {
     // displayCities();
     // fetchWeatherData();
     fetchLatLon(cityEntry);
+    getFiveDayForcast(cityEntry);
     cityName.textContent = cityEntry.toUpperCase();
 
-    var todayDate = moment().format('M/D/YYYY')
+    
     var date = document.querySelector("#date");
     date.textContent = todayDate;
 
@@ -90,6 +92,46 @@ function fetchLatLon(cityEntry) {
 
 
  });    
+}
+
+
+function getFiveDayForcast(searchVal) {
+
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${searchVal}&units=imperial&appid=8145ecb6e9f22712784b4a7964038388`)
+        .then(function(response) {
+            return response.json()
+        }).then(function(data) {
+            console.log("data", data)
+            for(var i = 0; i < 5; i++){
+               var icon = data.list[i].weather[0].icon
+                var image = $('<img />').attr({
+                    "src": "https://openweathermap.org/img/wn/"+ icon + ".png"
+                });
+                var divEl = `
+                <div class = "col-2 forecastBox">
+                    <p> ${moment().add(i, 'days').format('M/D/YYYY')}</p>
+                    <p>Temp: ${data.list[i].main.temp}F</p>
+                    <p>Humidity: ${data.list[i].main.humidity}%</p>
+                    <img> ${"src", image}</img>
+                </div>
+                `
+                // var dateValue =moment().add(i, 'days').format('M/D/YYYY');
+                // // <div class = "col-2 forecastBox">
+                // var tempValue = data.list[i].main.temp;
+                // var humidityValue = data.list[i].main.humidity;
+                    
+                                
+                // <p> $.attr("src", "https://openweathermap.org/img/wn/"+ icon + ".png")</p>
+                // var icon = data.list[i].weather[0].icon
+                
+                $("#five-day").append($(divEl))
+                // $("#five-day").append($(image))
+                // $("#weather-icons").append($(image))
+                // $(divEl).append($(image))
+            }
+        })
+
+
 }
 
 
