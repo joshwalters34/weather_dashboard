35680f12d868e74f5e914f2b9692f98c390366c5
JWalters <joshwalters34@gmail.com>
2021-03-06 23:13:44 -0500

    added readme and code to get the weather icon to display in the 5 day forecast

--- Develop/script.js
+++ Develop/script.js
@@ -25,8 +25,6 @@ $("#searchBtn").on("click", function (event) {
     searchText.value = "";
  
     storeCities();
-    // displayCities();
-    // fetchWeatherData();
     fetchLatLon(cityEntry);
     getFiveDayForcast(cityEntry);
     cityName.textContent = cityEntry.toUpperCase();
@@ -44,15 +42,10 @@ function fetchLatLon(cityEntry) {
 
     fetch ("https://api.openweathermap.org/data/2.5/weather?q=" + cityEntry + "&units=imperial&appid=8145ecb6e9f22712784b4a7964038388")
     .then(function(response) {
-        // console.log(response);
- //console.log(response.coord.lon);
- //console.log(response.coord.lat);
+ 
         return response.json();
     }) .then(function(results) {
         console.log(results);
-        console.log(results.coord.lon);
-        console.log(results.coord.lat);
-        console.log(results.main.temp);
         var lat = results.coord.lon;
         var lon = results.coord.lat;
         var tempEl = Math.round(results.main.temp);
@@ -104,49 +97,25 @@ function getFiveDayForcast(searchVal) {
             console.log("data", data)
             for(var i = 0; i < 5; i++){
                var icon = data.list[i].weather[0].icon
-                var image = $('<img />').attr({
-                    "src": "https://openweathermap.org/img/wn/"+ icon + ".png"
-                });
+                // var image = $('<img />').attr({
+                //     "src": "https://openweathermap.org/img/wn/"+ icon + ".png"
+                // });
+                var image = "https://openweathermap.org/img/wn/"+ icon + ".png"
                 var divEl = `
                 <div class = "col-2 forecastBox">
                     <p> ${moment().add(i, 'days').format('M/D/YYYY')}</p>
+                    <img src = ${image}></img>
                     <p>Temp: ${data.list[i].main.temp}F</p>
                     <p>Humidity: ${data.list[i].main.humidity}%</p>
-                    <img> ${"src", image}</img>
                 </div>
-                `
-                // var dateValue =moment().add(i, 'days').format('M/D/YYYY');
-                // // <div class = "col-2 forecastBox">
-                // var tempValue = data.list[i].main.temp;
-                // var humidityValue = data.list[i].main.humidity;
-                    
-                                
-                // <p> $.attr("src", "https://openweathermap.org/img/wn/"+ icon + ".png")</p>
-                // var icon = data.list[i].weather[0].icon
-                
+                `                
                 $("#five-day").append($(divEl))
-                // $("#five-day").append($(image))
-                // $("#weather-icons").append($(image))
-                // $(divEl).append($(image))
+                
             }
         })
 
-
 }
 
-
-//   function displayWeather(results){
-//     var weatherData = document.createElement('p');
-//         weatherData.classList.add('bg-light', 'text-dark', 'my-2')
-
-//         var tempData = document.createElement('p');
-//        tempData.classList.add('bg-ligh', 'text-dark', 'my-2');
-//        tempData.innerHTML = "Temperature: " + results.current.temp;
-//        weatherDisplay.append(tempdata)
-//        console.log(results.current.temp)
-//   }
-
-//   function to display history of cities on left side
 function displayCities() {
     cityHistory.innerHTML = "";
 
@@ -157,6 +126,13 @@ function displayCities() {
         list.setAttribute("data-index", i);
 
         recentCities.append(list);
+
+        $(this).on("click", function (event) {
+            event.preventDefault();
+
+            cityEntry = ;
+            fetchLatLon();
+        })
     }
 }
 // local storage get function to pull back recent cities
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# weather_dashboard
+# Weather_Dashboard
+This website is a site where users can go for up-to-date weather information on any city of their choosing.  My site shows the current conditions, including the UV Index, letting users know if they need some extra sunscreen and a hat.  Users can also see a 5-day forecast to know how to plan out there week.  
+
+## Motivation
+My motivation for this website was to create an easy way for users to see weather conditions for any city across the globe.  Whether you're just leaving your house for work heading or heading on a trip, knowing the weather is vital for your daily activities.  
+
+### Image of my website
+
+ ![Screenshot](./develop/images/weather-dashboard screenshot.png)
+
+### Website Address
+
+Visit my [Website](https://joshwalters34.github.io/weather_dashboard/)
+
+### Repository
+
+View my [GitHub_Repository](https://github.com/joshwalters34/weather_dashboard)
